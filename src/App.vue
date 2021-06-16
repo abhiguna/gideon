30b68cb7c41b851703a7193396c53aeadd5cdb8a
Mr. CEO <agunase@purdue.edu>
2021-06-16 13:05:33 -0500

    Done...

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <amplify-authenticator>
+    <div id="app">
+      <camera />
+    </div>
+    <div id="logout">
+      <amplify-sign-out></amplify-sign-out>
+    </div>
+  </amplify-authenticator>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import camera from "./components/camera.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    camera,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +27,39 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: #f0f0f0;
+}
+#video {
+  background-color: #000000;
+}
+#canvas {
+  display: none;
+}
+#logout {
+  width: 640px;
+  margin: auto;
+}
+#statusBar {
+  background-color: darkslategray;
+  margin: auto;
+  width: 640px;
+  text-emphasis: bold;
+  color: white;
+}
+li {
+  display: inline;
+  padding: 5px;
+}
+div {
+  padding: 10px;
+}
+button {
+  margin: 0 10px 20px 0;
+  width: 120px;
+}
+.hidden {
+  display: none;
 }
 </style>
